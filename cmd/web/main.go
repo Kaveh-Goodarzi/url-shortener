682b3374e1c8f064a52315247010a198b8930ad3
Kaveh-Goodarzi <kaveh4.voll@gmail.com>
2026-09-23 19:07:14 +0330

    change approach for connecting to db and move codes to a specific file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,37 +1,19 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net/http"
 
-	"github.com/Kaveh-Goodarzi/url-shortner/internal/config"
 	"github.com/Kaveh-Goodarzi/url-shortner/internal/handlers"
-	"github.com/jackc/pgx/v5"
 )
 
 func main() {
-	dbURL, err := config.Load()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	conn, err := pgx.Connect(context.Background(), dbURL)
-	if err != nil {
-		log.Fatal("unable to connect to database")
-	}
-	defer conn.Close(context.Background())
-
-	if err := conn.Ping(context.Background()); err != nil {
-		log.Fatal("database connection failed")
-	}
-
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("POST /urls", handlers.CreateURLHandler)
 	mux.HandleFunc("GET /{code}", handlers.RedirectURLHandler)
 
 	log.Println("starting server on localhost:8080")
-	err = http.ListenAndServe(":8080", mux)
+	err := http.ListenAndServe(":8080", mux)
 	log.Fatal(err)
 }
--- internal/database/database.go
+++ internal/database/database.go
@@ -1,3 +1,30 @@
 package database
 
+import (
+	"database/sql"
+	"log"
+
+	"github.com/Kaveh-Goodarzi/url-shortner/internal/config"
+	_ "github.com/jackc/pgx/v5"
+)
+
 var URLstore = make(map[string]string)
+
+var DB *sql.DB
+
+func InitDB() {
+	dbURL, err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	DB, err = sql.Open("pgx", dbURL)
+	if err != nil {
+		log.Fatal("unable to connect to database")
+	}
+
+	if err := DB.Ping(); err != nil {
+		log.Fatal("database connection failed")
+	}
+	defer DB.Close()
+}
